de9a00d96938b3ed1c57218a80c3c6859738bb35
GilHwan <dyghks123@gmail.com>
2022-02-11 18:47:01 +0900

    💄  UI component(editor,preview) style

---
+++ src/components/editor/editor.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './editor.module.css';
+
+const Editor = ({ props }) => {
+  return (
+    <>
+      <section className={styles.editor}>
+        <h1> Card Main </h1>
+      </section>
+    </>
+  );
+};
+
+export default Editor;
---
+++ src/components/preview/preview.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './preview.module.css';
+
+const Preview = (props) => {
+  return (
+    <section className={styles.preview}>
+      <h1>Pre view </h1>
+    </section>
+  );
+};
+
+export default Preview;
